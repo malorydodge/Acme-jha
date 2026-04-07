0ad8bd38f16b8e06a9035c9dea09af154d09290f
malorydodge <malorydodge@gmail.com>
2026-04-07 09:39:42 -0400

    fix order of initializing backend app. fix origins so there are no trailing slashes

--- backend/main.py
+++ backend/main.py
@@ -8,6 +8,21 @@ import os
 
 app = FastAPI()
 
+origins = [
+    "http://localhost:8080", 
+    "http://localhost:5173",
+    "https://acme-ayoyrtlrr-malory-dodges-projects.vercel.app",
+    "https://acme-jha-production.up.railway.app",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,         # Allows specific origins
+    allow_credentials=True,        # Allows cookies/auth headers to be sent
+    allow_methods=["*"],           # Allows all methods (GET, POST, PUT, DELETE, OPTIONS, etc.)
+    allow_headers=["*"],           # Allows all headers (Content-Type, Authorization, etc.)
+)
+
 UPLOAD_DIR = "uploads"
 
 os.makedirs(UPLOAD_DIR, exist_ok=True)
@@ -21,17 +36,3 @@ app.include_router(steps.router)
 app.include_router(hazards.router)
 app.include_router(controls.router)
 
-# origins = [
-#     "http://localhost:8080", 
-#     "http://localhost:5173",
-#     "https://acme-ayoyrtlrr-malory-dodges-projects.vercel.app/",
-#     "https://acme-jha-production.up.railway.app/",
-# ]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],           # Allows specific origins
-    allow_credentials=True,        # Allows cookies/auth headers to be sent
-    allow_methods=["*"],           # Allows all methods (GET, POST, PUT, DELETE, OPTIONS, etc.)
-    allow_headers=["*"],           # Allows all headers (Content-Type, Authorization, etc.)
-)
